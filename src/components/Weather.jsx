23a4c74ad9c661f73004e47ca586e0f35696f9a8
Ian Clemence <ianclemence17@gmail.com>
2022-05-20 12:23:14 +0300

    Create weather component

---
+++ src/components/Weather.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Weather = ({ cityWeather }) => {
+  const { name } = cityWeather || {};
+  const { icon } = cityWeather?.weather[0] || {};
+  const { temp, humidity } = cityWeather?.main || {};
+  const { speed } = cityWeather?.wind || {};
+
+  console.log(cityWeather);
+
+  return (
+    <>
+      <div className="weather loading">
+        <div className="location">
+          <h1 className="location-city">Weather in {name}</h1>
+          <img
+            src={`https://openweathermap.org/img/wn/${icon}.png`}
+            alt=""
+            className="icon"
+          />
+        </div>
+        <div className="temperature">
+          <div className="degree-section">
+            <h2 className="temperature-degree">
+              {temp}
+              <span>°C</span>
+            </h2>
+          </div>
+          <div className="temperature-des">Cloudy</div>
+          <div className="humidity">Humidity: {humidity}%</div>
+          <div className="wind">Wind: {speed}km/hr</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Weather;
