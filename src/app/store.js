2c84a86c3dac6de4fef26c7fc2740d61e5492efa
Ian Clemence <ianclemence17@gmail.com>
2022-05-20 12:22:52 +0300

    Create app store

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { weatherApi } from '../services/weatherApi';
+
+export default configureStore({
+  reducer: {
+    [weatherApi.reducerPath]: weatherApi.reducer,
+  },
+})
